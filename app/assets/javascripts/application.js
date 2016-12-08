ebdce6edb4b83c1dbea8047b34de356b3932bc7a
RUBY <RUBY@avalon.ru>
2016-12-08 21:12:37 +0300

    08.12.16

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,11 +7,11 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file. JavaScript code in this file should be added after the last require_* statement.
 //
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// Read Sprockets README (https://githu b.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //= require_self
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-// required bootstrap
+//= require bootstrap-sprockets
 //= require_tree .
